0287d9870e568f4cf0558401c53aae7e1c7b6158
cpstela <clara.peraferrer@grupobld.com>
2022-02-21 14:48:18 +0100

    Part1

---
+++ js-file.js
@@ -0,0 +1,19 @@
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case '+':
+          //Sumar
+            break;
+        case '-':
+          //Restar
+            break;
+        case '*':
+          //Multiplicar
+            break;
+        case '/':
+            //Dividir
+            break; 
+        default:
+          //Entra un operator vàlid
+            break;
+      }
+}
